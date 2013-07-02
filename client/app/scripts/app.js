0f18365aa020e047e539363603d6c1c013bc9990
Matt Miller <matt@facultycreative.com>
2013-07-02 08:58:51 -0400

    Show sidebar options based on user permissions.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -133,7 +133,7 @@ angular
       
     ;
   }])
-  .run(['$rootScope', '$location', function($rootScope, $location) {
+  .run(['$rootScope', '$location', 'authService', function($rootScope, $location, Auth) {
   
   
         // global functions and variables available app wide in $rootScope go here!
@@ -161,18 +161,27 @@ angular
                 return "";
             }
         };
-
-        // Handle updating page title
-    $rootScope.$on('$routeChangeSuccess', function($event, $route, $previousRoute) {
-        var pageSlug = $location.path().split('/');
-        //console.log(pageSlug[pageSlug.length - 1]);
-        $rootScope.pageSlug = pageSlug[pageSlug.length - 1];
         
-        if($rootScope.pageSlug.length === 0){
-            $rootScope.pageSlug = 'home';
-        }
-        //$rootScope.page_title = $route.$route && $route.$route.title ? base_title + ' | ' + $route.$route.title : base_title; 
-    });
+        
+        $rootScope.showIfUserCanDoAction = function(action) {
+            return Auth.showIfUserCanDoAction(action);  
+        };
+        
+        
+        // Handle updating page title
+        $rootScope.$on('$routeChangeSuccess', function($event, $route, $previousRoute) {
+            var pageSlug = $location.path().split('/');
+            //console.log(pageSlug[pageSlug.length - 1]);
+            $rootScope.pageSlug = pageSlug[pageSlug.length - 1];
+            
+            if($rootScope.pageSlug.length === 0){
+                $rootScope.pageSlug = 'home';
+            }
+            //$rootScope.page_title = $route.$route && $route.$route.title ? base_title + ' | ' + $route.$route.title : base_title; 
+        });
+        
+        
+        
   
   }])
   .directive('userTray', [ 'authService', '$location', function(Auth, $location) {

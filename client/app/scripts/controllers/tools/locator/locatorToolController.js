0b0b85b7602f14294d3e7439a61ccc434fb8aca1
Matt Miller <matt@facultycreative.com>
2013-07-09 18:10:14 -0400

    Semi-major updates to controller and html (See desc)
    - Added geo-location function from example
    - Hide geo location if not available
    - Added support for markers!!!!
    - Added text based location search
    - Removed the awkward $.extend($scope, {}) crap in favor of a more standard angular notation.
    - Various comments and formatting
    
    @note next steps should be including this geo data in the vendor service already.  Also performing location / distance based queries  on the markers.

--- client/app/scripts/controllers/tools/locator/locatorToolController.js
+++ client/app/scripts/controllers/tools/locator/locatorToolController.js
@@ -11,49 +11,27 @@ angular
        
        google.maps.visualRefresh = true;       
        
-       angular.extend($scope, {
-    
-            position: {
-              coords: {
-                latitude: 45,
-                longitude: -73
-              }
-            },
-            
-            /** the initial center of the map */
-            centerProperty: {
-                latitude: 45,
-                longitude: -73
-            },
-            
-            /** the initial zoom level of the map */
-            zoomProperty: 4,
-            
-            /** list of markers to put in the map */
-            markersProperty: [ {
-                    latitude: 45,
-                    longitude: -74
-                }],
-            
-            // These 2 properties will be set when clicking on the map
-            clickedLatitudeProperty: null,  
-            clickedLongitudeProperty: null,
-            
-            eventsProperty: {
-              click: function (mapModel, eventName, originalEventArgs) {    
-                // 'this' is the directive's scope
-                //console.log("user defined event on map directive with scope", this);
-                //console.log("user defined event: " + eventName, mapModel, originalEventArgs);
-              }
-            }
-        });
+       /** the initial center of the map */
+       
+        
+       
        
         // Gets all the vendors
         $scope.vendors = Vendor.getAll();
         
+        $scope.markers = [];
+        
+        // filteredVendors array is generated in the html view, and used to filter at the same time.
+        // we watch it for change
+        // on change, 
+        // - clear the markers array, 
+        // - loop through the filtered set, 
+        // - perform a geo location call on each
+        // this is realllyyyy costly on api calls, plus we are getting geo data repeatedly for the same vendors
+        // we should instead be saving lat / long with the vendor
         $scope.$watch('filteredVendors', function(){
              console.log($scope.filteredVendors);
-             $scope.markersProperty = [];
+             $scope.markers = [];
              for(var vendor in $scope.filteredVendors){
                  console.log(vendor);
                 var v = $scope.filteredVendors[vendor];
@@ -62,9 +40,82 @@ angular
              }
         }, true);
         
-        $rootScope.$on('event:geo-location-success', function(event, data) {
-            $scope.markersProperty.push(data);
-            $scope.$apply();
+        
+        // callback from the geo lookup
+        // the data returned will contain our coords
+        // @note this should be moved to the vendors settings tab
+        $rootScope.$on('event:geo-location-success', function(event, data, type) {
+            
+            // hack to support markers and location search with one callback! 
+            if(type && type === 'marker') {
+                data.infoWindow = 'This is an infowindow';
+                data.icon = 'https://maps.google.com/mapfiles/kml/shapes/schools_maps.png';
+                $scope.markers.push(data);
+                $scope.$apply(); 
+            } else if(type && type === 'locationSearch') {
+                
+                $scope.center = {
+                    latitude: data.latitude,
+                    longitude: data.longitude
+                };
+                
+                $scope.$apply();
+                
+            }
+            
+            
         });
+        
+        
+        // map events
+        // @note enents on markers dont register here! 
+        $scope.mapEvents = {
+          click: function (mapModel, eventName, originalEventArgs) {    
+            // 'this' is the directive's scope
+            console.log("user defined event on map directive with scope", this);
+            console.log("user defined event: " + eventName, mapModel, originalEventArgs);
+          }
+        };
+
+        
+        // default location
+        $scope.center = {
+            latitude: 45,
+            longitude: -73
+        };
+        
+        $scope.latitude = null;
+        $scope.longitude = null;
+        
+        $scope.zoom = 4;
+        
+        
+        
+        
+        
+        // check for geo location support
+        $scope.geolocationAvailable = navigator.geolocation ? true : false;
+        
+        // @todo we should add a marker here
+        $scope.findMe = function () {
+            
+            if (!$scope.geolocationAvailable) return false;
+                
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $scope.center = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+                $scope.zoom = 16;
+                $scope.$apply();
+            });
+        };
+        
+        
+        $scope.findMyLocation = function() {
+            console.log($scope.locationSearch);  
+            googleMaps.geo($scope.locationSearch, 'locationSearch');
+        };
+        
     }
   ]);

687ebe026469d899eb2f6e594595206560a498ec
Matt Miller <matt@facultycreative.com>
2013-07-16 12:02:46 -0400

    Simple api structure for wines

--- server/lib/api/index.js
+++ server/lib/api/index.js
@@ -2,6 +2,7 @@ var express   = require('express');
 var app       = module.exports = express();
 var fs        = require('fs');
 var path      = require('path');
+var wines     = require('./routes/wines');
 
 app.get('/api/bower', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../bower.json'));
@@ -14,3 +15,6 @@ app.get('/api/package', function(req, res) {
 app.get('/api/changelog', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../changelog.md'));
 });
+
+app.get('/api/wines', wines.findAll);
+app.get('/api/wines/:id', wines.findById);

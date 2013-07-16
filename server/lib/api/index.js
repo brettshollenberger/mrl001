944c4405ec8b365fb47a6e95e743a609452faf3d
Matt Miller <matt@facultycreative.com>
2013-07-16 12:14:05 -0400

    Added DB connection and all rest routes.

--- server/lib/api/index.js
+++ server/lib/api/index.js
@@ -2,7 +2,7 @@ var express   = require('express');
 var app       = module.exports = express();
 var fs        = require('fs');
 var path      = require('path');
-var wines     = require('./routes/wines');
+var wine     = require('./routes/wines');
 
 app.get('/api/bower', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../bower.json'));
@@ -16,5 +16,8 @@ app.get('/api/changelog', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../changelog.md'));
 });
 
-app.get('/api/wines', wines.findAll);
-app.get('/api/wines/:id', wines.findById);
+app.get('/api/wines', wine.findAll);
+app.get('/api/wines/:id', wine.findById);
+app.post('/api/wines', wine.addWine);
+app.put('/api/wines/:id', wine.updateWine);
+app.delete('/api/wines/:id', wine.deleteWine);

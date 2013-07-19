16536aa58854de53a810592b5de782ca7258743b
Matt Miller <matt@facultycreative.com>
2013-07-19 09:48:43 -0400

    [feature] added seed function and sample seed data.

---
+++ server/seed.js
@@ -0,0 +1,62 @@
+console.log('SERVER : INIT : Seeder');
+
+// our master resources object
+var resources = {};
+
+resources.vendor = require('./seed_data/vendor').seed();
+resources.program = require('./seed_data/program').seed();
+
+
+var insertResource = function(resource) {
+    
+    // get the collection
+    db.collection(resource, function(err, collection) {
+        // drop the collection
+        collection.drop(function(err, reply) {
+           console.log('SEED : STATUS : Drop ' + resource + ' Collection');
+        });
+        // insert the collection
+        collection.insert(resources[resource], {w:1}, function(err, result) {
+           console.log('SEED : STATUS : Create ' + resource + ' Collection');
+           console.log('SEED : STATUS : finished seeding ' + resource);
+           console.log('SEED : SUCCESS : Quitting process');
+           console.log('------------------------------------------------------------');
+           process.exit(0);
+        });
+        
+        
+    });
+};
+
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('marlindb', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log('------------------------------------------------------------');
+        console.log("Connected to 'marlindb' database");
+        console.log("Seeding the Database");
+        populateDB();
+    } else {
+        console.log(err);
+    }
+});
+
+var populateDB = function() {
+    console.log(resources);
+    
+    console.log('SEED : STATUS : beginning the seed process... cross your fingers!');
+    // iterate over all of the resources to add to database
+    for (var resource in resources) {
+        console.log('SEED : STATUS : starting with ' + resource);
+        insertResource(resource);
+    }    
+};
+
+
---
+++ server/seed_data/vendor.js
@@ -0,0 +1,139 @@
+exports.seed = function() {
+    return data;
+}
+
+var data = [{
+    name: 'BearCom Operating LLC',
+    contactPerson: {
+        name: 'Jenn Delong',
+        email: 'jdelong@marlinfinance.com',
+        phone: '888-479-9111 Ext. 5015'
+    },
+    logo: {
+        original: 'https://www.filepicker.io/api/file/2v44yfa8TqKt2RAAkpZB'  
+    },
+    website: 'http://www.bearcom.com/',
+    legalTerms: 'These are legal terms for vendor 1',
+    businessPhone: '',
+    businessAddress: {
+        address1: '4009 Distribution Dr.',
+        address2: 'Bldg 200',
+        city: 'Garland',
+        state: 'TX',
+        zip: '75041'
+    },
+    programIds: [1,3,4], // @note these need to match the GUIDs for programs
+    //salesRep: User.getOneWhereIn('vendorIds', 1),
+    programs: [{
+        id: 1,
+        displayName: 'A Custom Display Name for Program 1'
+    }]
+},
+{
+    name: 'Protection One - West',
+    contactPerson: {
+        name: 'Jennifer DeLong-Giefer',
+        email: 'jdelong@marlinfinance.com',
+        phone: '866-687-3778'
+    },
+    logo: {
+        original: 'https://www.filepicker.io/api/file/ZxTd0qV0QA2ek9HHSmu3'  
+    },
+    website: 'http://www.protection1.com/',
+    legalTerms: 'These are legal terms for vendor 2',
+    businessPhone: '',
+    businessAddress: {
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: ''
+    },
+    programIds: [1,3,4],
+    //salesRep: User.getOneWhereIn('vendorIds', 1),
+    programs: [{
+        id: 1,
+        displayName: 'A Custom Display Name for Program 1'
+    }]
+},
+{
+    name: 'Gametime',
+    contactPerson: {
+        name: 'Warren Myers',
+        email: 'wmyers@playcore.com',
+        phone: ''
+    },
+    logo: {
+        original: 'https://www.filepicker.io/api/file/EcryKofERBynAuL2txez'  
+    },
+    website: 'http://www.gametime.com',
+    legalTerms: 'These are legal terms for vendor 3',
+    businessPhone: '',
+    businessAddress: {
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: ''
+    },
+    programIds: [1,3,4],
+    //salesRep: User.getOneWhereIn('vendorIds', 1),
+    programs: [{
+        id: 1,
+        displayName: 'A Custom Display Name for Program 1'
+    }]
+},
+{
+    name: 'Union Bank',
+    contactPerson: {
+        name: 'Union Bank Financing Consultant',
+        email: 'ubapps@marlinfinance.com',
+        phone: '877-307-6756'
+    },
+    logo: {
+        original: 'https://www.filepicker.io/api/file/yjlOg28UROmPXyK8uadA'  
+    },
+    website: 'https://www.unionbank.com/',
+    legalTerms: 'These are legal terms for vendor 3',
+    businessPhone: '',
+    businessAddress: {
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: ''
+    },
+    programIds: [1,3,4],
+    //salesRep: User.getOneWhereIn('vendorIds', 1),
+    programs: [{
+        id: 1,
+        displayName: 'A Custom Display Name for Program 1'
+    }]
+},
+{
+    name: 'Southeastern Equipment & Supply, Inc.',
+    contactPerson: {
+        name: 'Grady Martin',
+        email: 'leasing@southeasternequipment.net',
+        phone: '803-454-3656'
+    },
+    logo: {
+        original: 'https://www.filepicker.io/api/file/nvC3zhiTQoSl01P7K3Br'  
+    },
+    website: 'https://www.unionbank.com/',
+    legalTerms: 'These are legal terms for vendor 3',
+    businessPhone: '',
+    businessAddress: {
+        address1: '1919 Old Dunbar Road',
+        address2: '',
+        city: 'West Columbia',
+        state: 'SC',
+        zip: '29045'
+    },
+    programIds: [1,3,4],
+    //salesRep: User.getOneWhereIn('vendorIds', 1),
+    programs: [{
+        id: 1,
+        displayName: 'A Custom Display Name for Program 1'
+    }]
+}];

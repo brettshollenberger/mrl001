bf95becd0cf210b51454a0cc5bd72db8c4089dc4
Matt Miller <matt@facultycreative.com>
2013-07-22 12:37:34 -0400

    Added more validation

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -215,6 +215,11 @@ angular
                     }
                 }
                 
+                if(theCharacter.search(/\0/)===-1 && theCharacter.search(/\./)===-1 && ele.val().split(".").length === 1) {
+                    console.log('User is trying to enter a non-decimal number');
+                    e.preventDefault();
+                }
+                
                 if(ele.val().search(/\d+\.\d{3}/)===0 && newVal.length>ele.val().length){
                     e.preventDefault();
                 }
@@ -224,6 +229,21 @@ angular
                 }
                 
                 
+            });
+        }
+    };
+   
+   })
+   .directive('numberOnly',function(){
+    return {
+        link:function(scope,ele,attrs){
+            ele.bind('keypress',function(e){
+                var newVal=ele.val()+(e.charCode!==0?String.fromCharCode(e.charCode):'');
+                var theCharacter = String.fromCharCode(e.charCode);
+                
+                if(theCharacter.search(/\d/)===-1) {
+                    e.preventDefault();
+                }
             });
         }
     };

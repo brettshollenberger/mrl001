86faf8443a0480064cd3648ca1583326dcfaea00
Aaron <aaron@facultycreative.com>
2013-07-23 10:54:28 -0400

    Added a third program to the seed data

--- server/seed_data/program.js
+++ server/seed_data/program.js
@@ -74,4 +74,39 @@ var data = [{
                     }]
                 }]
         }
+},
+{
+    _id: new BSON.ObjectID('51e71518ed32080ffc000007'),
+    name: 'Program numero tres, a Yearly plan',
+    rateSheet: {
+        termPeriod: 'Year',
+        buyoutOptions: [
+                { 
+                name: '$1.00 Buyout Program', 
+                terms: [{length: 1}, {length: 2}, {length: 3}],
+                costs: [
+                    {
+                        min: 1000,
+                        max: 2000,
+                        rates: [{rate: 0.96}, {rate: 0.80}, {rate: 0.75}]
+                    },
+                    {
+                        min: 2001,
+                        max: 10000,
+                        rates: [{rate: 0.96}, {rate: 0.80}, {rate: 0.75}]
+                    }
+                    ]
+                },
+                { 
+                name: '10% Purchase Option', 
+                terms: [{length: 1}, {length: 2}, {length: 3}, {length: 4}],
+                costs: [
+                    {
+                        min: 1000,
+                        max: 2000,
+                        rates: [{rate: 0.96}, {rate: 0.80}, {rate: 0.75}, {rate: 0.75}]
+                        
+                    }]
+                }]
+        }
 }];

ad11fdbef54460a8be635bb26e092eb48622b6e7
Matt Miller <matt@facultycreative.com>
2013-08-13 13:11:09 -0400

    Converted user edit page to work with unsaved changes prompt.

--- client/app/scripts/controllers/users/userEdit.js
+++ client/app/scripts/controllers/users/userEdit.js
@@ -8,9 +8,17 @@ angular
     'authService',
     'userService',
     'vendorService',
-    function($rootScope, $scope, $location, $routeParams, Auth, User, Vendor) {
+    'saveChangesPrompt',
+    function($rootScope, $scope, $location, $routeParams, Auth, User, Vendor, saveChangesPrompt) {
        
         Auth.canUserDoAction('edit-user');
+        
+        var removeViewLoad = $rootScope.$on('$viewContentLoaded', function() {
+            // this will prompt users to save when the leave the page. 
+            var forms = [$scope.basicForm, $scope.passwordForm];
+            saveChangesPrompt.init(forms); 
+            removeViewLoad();
+        });
        
         // empty user object
         $scope.user = {};
@@ -70,16 +78,26 @@ angular
                    $scope.user = response;
                    // this ensures that on the next save, vendorId is set and the previous if() doesnt run
                    userId = $scope.user._id;
+                   saveChangesPrompt.removeListener();
+                   
+                    if(doRedirect) {
+                       $location.url('/dashboard/users'); 
+                    }
+                   
                 });
                 
             } else {
                 // update existing item
                 User.update($scope.user);
+                
+                saveChangesPrompt.removeListener();
+                
+                if(doRedirect) {
+                   $location.url('/dashboard/users'); 
+                }
             }
             
-            if(doRedirect) {
-                $location.url('/dashboard/users'); 
-            }
+            
         };
 
         

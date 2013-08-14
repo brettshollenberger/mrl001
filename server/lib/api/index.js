37df9848843fdfa9ed6085a4b88e6fb0bc50171a
Matt Miller <matt@facultycreative.com>
2013-08-13 22:40:54 -0400

    Added webshot PDF generation module and dynamic route.

--- server/lib/api/index.js
+++ server/lib/api/index.js
@@ -5,6 +5,7 @@ var fs        = require('fs');
 var passport  = require('passport');
 var path      = require('path');
 var resource  = require('./routes/resource');
+var webshot = require('webshot');
 
 resource.setup(app);
 
@@ -19,6 +20,34 @@ app.get('/api/package', function(req, res) {
 app.get('/api/changelog', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../changelog.md'));
 });
+
+app.get('/api/quote/:id/pdf', function(req, res) {
+    
+    console.log('generating pdf for quote id: ' + req.params.id);
+    
+    var url = 'http://localhost:3000/#/tools/quoter/' + req.params.id;
+    var fileName = 'temp/' + req.params.id + '.pdf';
+    
+    var options = {
+        screenSize: {
+            width: 320,
+            height: 480
+        },
+        shotSize: {
+            width: 320,
+            height: 'all'
+        },
+        streamType: 'pdf',
+        paperSize: {format: 'letter', orientation: 'portrait'},
+        userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)' + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+    };
+    webshot(url, fileName, options, function(err) {
+        console.log('OK');
+        res.download(path.resolve(__dirname + '/../../../' + fileName));
+    });
+});
+
+
 
 /*
 app.get('/api/me/auth', 

9fa993bada61abb55e5b35f150b8d92017f4bdf5
Matt Miller <matt@facultycreative.com>
2013-09-02 11:44:41 -0400

    Added ISO data directive (Example, not used)

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -684,6 +684,18 @@ angular.module('app').config(function($compileProvider){
   $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|callto):/);
 });
 
+
+
+angular.module('app') // attach to your app or create a new with ('filters', [])
+.filter('isodate', function(){
+   return function(datetime){
+       var n = datetime.split(' ');
+       if(n.length == 1) return datetime;
+       else return n.join('T')+'Z';
+   };
+});
+
+
 angular.module('app')
 .directive('stopEvent', function () {
     return {

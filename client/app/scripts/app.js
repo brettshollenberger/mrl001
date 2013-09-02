f7c2d1424176fccc2ce75629bbd675df18eb7f8b
Matt Miller <matt@facultycreative.com>
2013-09-02 11:43:31 -0400

    Added directive to stop event propagation

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -677,3 +677,14 @@ spinner.directive("spinner", function() {
         controller: "spinner"
     };
 });
+angular.module('app')
+.directive('stopEvent', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            element.bind(attr.stopEvent, function (e) {
+                e.stopPropagation();
+            });
+        }
+    };
+});

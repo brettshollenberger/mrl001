28929b9e50cb38a720dea5887ff5f3168d78a99c
Matt Miller <matt@facultycreative.com>
2013-09-02 12:09:50 -0400

    Created directives for mailto, call to, and user profile.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -707,3 +707,52 @@ angular.module('app')
         }
     };
 });
+
+
+
+
+
+angular
+.module('app')
+.directive("mailTo", function() {
+    return {
+        replace: true,
+        template: '<a ng-show="email" stop-event="click" ng-href="mailto:{{email}}" target="_blank"><i class="icon icon-envelope"></i> {{email}}</a>',
+        link: function(scope, element, attrs) {
+            
+            attrs.$observe('mailTo', function(item) {
+                scope.email = item;
+            });
+                        
+        }
+    };
+})
+.directive("callTo", function() {
+    return {
+        replace: true,
+        template: '<a ng-show="phone" stop-event="click" ng-href="callto:{{phone}}"><i class="icon icon-phone"></i> {{phone}}</a>',
+        link: function(scope, element, attrs) {
+            
+            attrs.$observe('callTo', function(item) {
+                scope.phone = item;
+            });
+                        
+        }
+    };
+})
+.directive("userProfile", function() {
+    
+    return {
+        replace: true,
+        template: '<a stop-event="click" ng-href="#/dashboard/users/{{userProfile()._id}}"><i class="icon icon-user"></i> {{userProfile().fullname}}</a>',
+        scope: {
+            userProfile: '&'
+        }
+    };
+})
+
+;
+
+
+
+

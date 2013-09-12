1b0258c207918d0e82c6fc0396ba8e855783b689
Matt Miller <matt@facultycreative.com>
2013-09-12 11:21:19 -0400

    Add service function, route, and controller to get a vendor.
    Since the api automatically populates req.vendor when a vendorId is present in the url, we just need to grab them salesRep (which has already been populated) and send.

--- client/app/scripts/services/vendor.js
+++ client/app/scripts/services/vendor.js
@@ -72,6 +72,13 @@ angular.module('app').factory('vendorService', ['$http', 'MARLINAPI_CONFIG', 'us
                 return response.data;
             });
         };
+        
+        // get one item by id
+        exports.getSalesRep = function(vendorId) {
+            return $http.get(url + 'vendors/' + vendorId + '/vendorRep').then(function(response) {
+                return response.data;
+            });
+        };
 
         /**
          * Add a vendor to a program using the id of each

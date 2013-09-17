8c0b2b5859317faf0b1a1f9baa98df20ab2629df
Matt Miller <matt@facultycreative.com>
2013-09-17 11:27:29 -0400

    Move filter to filters/ folder.

--- client/app/scripts/directives/uiHelpers.js
+++ client/app/scripts/directives/uiHelpers.js
@@ -48,13 +48,6 @@ angular
             }
         };
     })
-    .filter('isodate', function() {
-        return function(datetime) {
-            var n = datetime.split(' ');
-            if (n.length == 1) return datetime;
-            else return n.join('T') + 'Z';
-        };
-    })
     .directive('stopEvent', function() {
         return {
             restrict: 'A',

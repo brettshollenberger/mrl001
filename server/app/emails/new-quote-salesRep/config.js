0e17b521bdda0bcedccbb782ae5e237ebb369f8a
Matt Miller <matt@facultycreative.com>
2013-09-19 17:33:43 -0400

    Add salesRep email template and config.

---
+++ server/app/emails/new-quote-salesRep/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    defaults: {
+    
+        // Subject of the message
+        subject: 'Thankf for generating a quote',
+    
+        // variables 
+        variables: {
+            link: '',           // link to dashboard quote
+            salesRepName: '',   
+            vendorName: '',
+            dateTime: ''
+        }
+    }
+};

47bd19d9dee65cd171bc31503c365af33facd525
Matt Miller <matt@facultycreative.com>
2013-09-19 17:31:44 -0400

    Add missing load() static to User model.

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -174,4 +174,15 @@ UserSchema.methods = {
     }
 };
 
+/**
+ * Statics
+ */
+UserSchema.statics = {
+    load: function(id, cb) {
+        this.findOne({
+            _id: id
+        }).exec(cb);
+    }
+};
+
 mongoose.model('User', UserSchema);

eda66c28baddc2c5dd717f88783c24de8b6c8b6f
Matt Miller <matt@facultycreative.com>
2013-09-19 17:31:26 -0400

    Set virtual field on quote schema that gives us access to quoter tool link.

--- server/app/models/quote.js
+++ server/app/models/quote.js
@@ -63,6 +63,13 @@ var QuoteSchema = new Schema({
     }
 });
 
+
+// virtual fields are available anywhere on the server. 
+// They are not passes over the API unless explicitly set. 
+QuoteSchema.virtual('quoterToolLink').get(function() {
+    return config.siteUrl + '/tools/quoter/' + this._id;
+});
+
 /*
 // the below 4 validations only apply if you are signing up traditionally
 QuoteSchema.path('vendorId').validate(function(vendorId) {

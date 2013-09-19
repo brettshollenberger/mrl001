deee8c97528404e500e77b8e903fd8298941e025
Matt Miller <matt@facultycreative.com>
2013-09-19 17:33:58 -0400

    Add end user (requestor) email template and config.

---
+++ server/app/emails/new-quote-requester/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    defaults: {
+    
+        // Subject of the message
+        subject: 'Thankf for generating a quote',
+    
+        // variables 
+        variables: {
+            link: '', // link to use quoter tool
+            fullName: ''
+        }
+    }
+};

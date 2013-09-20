b2945e0bcd62eff0c18b0ad9c5b00861891fe28b
Matt Miller <matt@facultycreative.com>
2013-09-20 14:22:57 -0400

    Fix seed data.

--- server/seed_data/quote.js
+++ server/seed_data/quote.js
@@ -3,88 +3,91 @@ exports.seed = function() {
 };
 
 // dummy data
-var data = [/* 0 */
-{
-    "vendorRep" : "51e71518ed32080ffc000030",
-    "salesRep" : "51e71518ed32080ffc000016",
-    "_id" : "51e71518ed32080ffc000017",
-    "vendorId" : "51e71518ed32080ffc000023",
-    "company" : {
-        "zip" : "",
-        "state" : "PA",
-        "city" : "Philadelphia",
-        "address2" : "",
-        "address1" : "123 Company Lane",
-        "name" : "NASA"
-    },
-    "description" : "I need new rockets for my space shuttle booster.",
-    "status" : "Open",
-    "totalCost" : 1000,
-    "__v" : 0
+var data = [{
+    _id: '51e71518ed32080ffc000017',
+    totalCost: 1000,
+    status: 'Open',
+    vendorId: '51e71518ed32080ffc000023',
+    description: 'I need new rockets for my space shuttle booster.',
+    company: {
+        fullLegalBusinessName: 'Max Rockets',
+        contactPerson: {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            phone: '556-669-4444'
+        },
+        businessAddress: {
+            address1: '2424 York Street',
+            address2: '',
+            city: 'Philadelphia',
+            state: 'PA',
+            zip: '19125'
+        }
+    }
 },
 {
-    "salesRep" : "51e71518ed32080ffc000016",
-    "_id" : "51e71518ed32080ffc000018",
-    "vendorId" : "51e71518ed32080ffc000024",
-    "company" : {
-        "zip" : "",
-        "state" : "PA",
-        "city" : "Philadelphia",
-        "address2" : "",
-        "address1" : "123 Company Lane",
-        "name" : "Art Manaquins"
-    },
-    "description" : "Manaquins for my art project in the SkyBox",
-    "status" : "Open",
-    "totalCost" : 9900,
-    "__v" : 0
-},{
-    "salesRep" : "51e71518ed32080ffc000010",
-    "_id" : "51e71518ed32080ffc000019",
-    "vendorId" : "51e71518ed32080ffc000025",
-    "company" : {
-        "zip" : "",
-        "state" : "PA",
-        "city" : "Philadelphia",
-        "address2" : "",
-        "address1" : "123 Company Lane",
-        "name" : "C is for Cookie"
-    },
-    "description" : "Cookies for lunch for everyone!",
-    "status" : "Archived",
-    "totalCost" : 2000,
-    "__v" : 0
-},{
-    "salesRep" : "51e71518ed32080ffc000010",
-    "_id" : "51e71518ed32080ffc000020",
-    "vendorId" : "51e71518ed32080ffc000025",
-    "company" : {
-        "zip" : "",
-        "state" : "PA",
-        "city" : "Philadelphia",
-        "address2" : "",
-        "address1" : "123 Company Lane",
-        "name" : "NSA"
-    },
-    "description" : "Security system for my office.",
-    "status" : "Archived",
-    "totalCost" : 28000,
-    "__v" : 0
-},{
-    "vendorRep" : "51e71518ed32080ffc000030",
-    "salesRep" : "51e71518ed32080ffc000016",
-    "vendorId" : "51e71518ed32080ffc000023",
-    "_id" : "5239e9075204f7bae0000005",
-    "company" : {
-        "zip" : "",
-        "state" : "",
-        "city" : "",
-        "address2" : "",
-        "address1" : "",
-        "name" : ""
-    },
-    "description" : "Complete warehouse radio overall, including all new Sonetics base stations, headsets, and intercoms.",
-    "status" : "Open",
-    "totalCost" : 402012,
-    "__v" : 0
+    _id: '51e71518ed32080ffc000018',
+    totalCost: 9900,
+    status: 'Open',
+    vendorId: '51e71518ed32080ffc000024',
+    description: 'Manaquins for my art project in the SkyBox',
+    company: {
+        fullLegalBusinessName: 'Max Fridge',
+        contactPerson: {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            phone: '556-669-4444'
+        },
+        businessAddress: {
+            address1: '2424 York Street',
+            address2: '',
+            city: 'Philadelphia',
+            state: 'PA',
+            zip: '19125'
+        }
+    }
+},
+{
+    _id: '51e71518ed32080ffc000019',
+    totalCost: 2000,
+    status: 'Archived',
+    vendorId: '51e71518ed32080ffc000025',
+    description: 'Cookies for lunch for everyone!',
+    company: {
+        fullLegalBusinessName: 'Cookie Company',
+        contactPerson: {
+            name: 'John Doe Cookie',
+            email: 'john@doe.com',
+            phone: '556-669-4444'
+        },
+        businessAddress: {
+            address1: '2424 York Street',
+            address2: '',
+            city: 'Philadelphia',
+            state: 'PA',
+            zip: '19125'
+        }
+    }
+},
+{
+    _id: '51e71518ed32080ffc000020',
+    totalCost: 28000,
+    status: 'Archived',
+    vendorId: '51e71518ed32080ffc000025',
+    description: 'Security system for my office.',
+    company: {
+        fullLegalBusinessName: 'Security Company',
+        contactPerson: {
+            name: 'John Doe Security',
+            email: 'john@doe.com',
+            phone: '556-669-4444'
+        },
+        businessAddress: {
+            address1: '2424 York Street',
+            address2: '',
+            city: 'Philadelphia',
+            state: 'PA',
+            zip: '19125'
+        }
+    }
 }];

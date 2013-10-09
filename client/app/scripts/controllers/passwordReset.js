05fac35eecada59bc252c8666997b7da907002f1
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-09 18:20:51 -0400

    Add controller & template for password reset form + find method for user service for more flexibility finding a user by email. This required a change of the backend user API all service to look for a query passed, but to default to no query passed, returning all the records. Backend API change coming as part of next commit.

---
+++ client/app/scripts/controllers/passwordReset.js
@@ -0,0 +1,32 @@
+angular
+    .module('app')
+    .controller('passwordResetController', [
+        '$scope',
+        'userService',
+        '$http',
+        function($scope, User, $http) {
+            $scope.isProcessing = false;
+            $scope.email = "";
+
+            $scope.passwordReset = function() {
+                $scope.isProcessing = true;
+
+                User.find({email: $scope.email}).then(function(response) {
+                    user = response;
+                    if (user && user !== null && user.email) {
+                        $http({
+                            method: 'GET',
+                            url: '/api/v1/users/' + user._id + '/reset_password'
+                        }).success(function(data) {
+                            $scope.message = "An email has been sent to " + $scope.email + " containing login instructions.";
+                        }).error(function(data) {
+                            $scope.message = "There was an error resetting your password. Please try again.";
+                        });
+                    } else {
+                        $scope.message = "We didn't find that email address in our system. Double check the spelling and try again";
+                    }
+                    $scope.isProcessing = false;
+                });              
+            };
+        }
+    ]);

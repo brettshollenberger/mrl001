a833da8cf67f9d24473defd28be42c54a50a76f9
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-11 14:19:14 -0400

    Refactor common controller save interface out to strategy pattern in CommonInterface service. Controllers that use this service can call any controller-specific functionality via a strategy method.

---
+++ client/app/scripts/services/commonInterface.js
@@ -0,0 +1,67 @@
+angular
+    .module('app')
+    .factory('CommonInterface', function($location, FormHelper) {
+        return {
+            save: function(options) {
+                // Strategy pattern; hand in an object to perform local
+                // pre-save setup, if necessary
+                var strategy    = options.strategy || function() {};
+
+                // Service object supporting thenable interface with add & save methods
+                var Model       = options.Model;
+
+                // The local instance of this model 
+                var instance    = options.instance;
+
+                // The routeParams id
+                var id          = options.id;
+
+                // The form to reset to pristine after save
+                var form        = options.form;
+
+                // The redirect URL, if doRedirect is set to true
+                var redirectUrl = options.redirectUrl;
+
+                // Whether or not to redirect. If false, the save method will set the form
+                // to pristine and stay on the same page. 
+                var doRedirect  = options.doRedirect || false;
+
+                // ********** Private Methods ************ //
+
+                function postSaveLogic() {
+                    if (doRedirect) {
+                        $location.url(redirectUrl);
+                    } else {
+                        FormHelper.setPristine(form);
+                    }
+                }
+
+                // ******** Execute the Strategy ******** //
+
+                strategy();
+
+                // ********* Perform Model.add ********* //
+                // ******** if not on show page ******** //
+
+                if (!id) {
+
+                    Model.add(instance).then(function(response) {
+                        instance = response;
+                        id       = instance._id;
+                        postSaveLogic();
+                    });
+
+                // ******* Perform Model.update ******** //
+                // ********* if on show page ********** //
+
+                } else {
+
+                    Model.update(instance).then(function(response) {
+                        instance = response;
+                        postSaveLogic();
+                    });
+
+                }
+            }
+        };
+    });

462c9181156f6dd2cd6e670f7ae8db38898c8902
Matt Miller <matt@facultycreative.com>
2013-10-14 00:00:12 -0400

    Move number toCents conversion into currency helper.
    This allows us to use in any controller.

--- server/app/controllers/quotes.js
+++ server/app/controllers/quotes.js
@@ -9,7 +9,8 @@ var mongoose = require('mongoose'),
     _ = require('underscore'),
     natural = require('natural'),
     nounInflector = new natural.NounInflector(),
-    numeral = require('numeral');
+    numeral = require('numeral'),
+    currency = require('./../helpers/currency');
 
 
 /**
@@ -54,39 +55,12 @@ exports.validateQuoteRequest = function(req, res, next) {
         return res.failure("description is required", 400);
     } 
     
-    // convert cost to string and save as variable for processing
-    var totalCost = req.body.totalCost.toString();
-    
-    // split to make sure there are max 2 parts, such as 000.00
-    var splitNumber = totalCost.split('.');
-    
-    // check for proper decimal places and only 1 decimal place if any
-    if(splitNumber.length > 2 || splitNumber[1] && splitNumber[1].length > 2) {
-       return res.failure("Invalid totalCost. Must be in for format 1000.00 or 1000. Seiding one decimal place, such as 1000.1 is OK too and will be processed ad 1000.10.", 400); 
-    } 
-    
-    // strip non-number junk from our value
-    // this also strips trailing 0's, so we'll add them back on in the next step
-    totalCost = numeral().unformat(totalCost);
-    //console.log('numeral().unformat(totalCost) = ', totalCost);
-    
-    // formats number with 2 decimal places, so 100.1 becomes 100.10
-    totalCost = numeral(totalCost).format('0.00').toString();
-    //console.log('numeral(totalCost).format() = ', totalCost);
-    
-    // replace the decimal, making the number in cents
-    // we do this instead of number * 100 because this can cause rounding issues
-    totalCost = totalCost.replace('.', '');
-    //console.log('Final totalCoast in cents ', totalCost);
-    
-    // convert back into number. There shouldn't be an issue with decimal places
-    // at this point because we'll have an integer and not a float
-    totalCost = parseInt(totalCost, 10);
-    
-    // lastly assign back to req.body.totalCost
-    req.body.totalCost = totalCost;
-    
-    next();
+    // format total cost toCents, supporting error callback if conversion fails
+    currency.toCents(req.body.totalCost, function(err, result) {
+        if(err) return res.failure(err, 400);
+        req.body.totalCost = result;
+        next();
+    });
     
 };
 

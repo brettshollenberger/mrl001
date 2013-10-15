a25204c06b815f1d4d4c47a4eec003a529c0ee57
Aaron Frey <aaron@facultycreative.com>
2013-10-15 14:37:35 -0400

    Reinitialize the map so the new marker is displayed when a user changes an address

--- client/app/scripts/controllers/vendors/vendorEdit.js
+++ client/app/scripts/controllers/vendors/vendorEdit.js
@@ -568,11 +568,9 @@ angular
              *
              */
             var listener = $rootScope.$on('event:geo-location-success', function(event, data, type) {
+                
                 // update center based on search 
                 if (type && type === 'locationSearch') {
-
-                    console.log('RETURN GEO DATA IS: ');
-                    console.log(data);
 
                     $scope.map.center = {
                         latitude: data.lat,
@@ -591,7 +589,6 @@ angular
                     $scope.$$childTail.basicForm.$setDirty();
 
                     $scope.$apply();
-
                 }
             });
 
@@ -638,6 +635,8 @@ angular
 
             function makeMarkerFromVendor() {
 
+                $scope.mapActive = false;
+
                 // build marker object from vendor info
                 // @note this is duplicate code from locator tool, move to service? 
                 // we need to create the marker from the vendor
@@ -659,17 +658,10 @@ angular
 
                 $scope.vendorMarker = [newMarker];
 
-                //console.log('VENDOR MARKER is...');
-                //console.log($scope.vendorMarker);
-
-                // doesnt seem to be needed
-                // was an attempt to get the map to re render
-                //
-                //var win = angular.element(window);
-                //$timeout(function() {
-                //    win.triggerHandler('Resize');
-                //});
-
+                // reshow the map so the new marker is displayed
+                $timeout(function() {
+                    $scope.mapActive = true;
+                });
             }
 
             /**

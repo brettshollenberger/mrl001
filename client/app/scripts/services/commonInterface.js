8c949db51efe0b355b760fcad0a70d5c1ca8f47f
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-15 18:10:45 -0400

    Refactor CommonInterface service to accept a pre-save hook & post-save hook. Handle array & object versions of the forms separately. Improve readability.

--- client/app/scripts/services/commonInterface.js
+++ client/app/scripts/services/commonInterface.js
@@ -4,73 +4,77 @@ angular
         return {
             save: function(options) {
                 // Strategy pattern; hand in an object to perform local
-                // pre-save setup, if necessary
-                var strategy    = options.strategy || function() {};
+                // pre-save setup or post-save breakdown, if necessary
+                var preSaveHook  = options.preSaveHook  || options.strategy || function() {};
+                var postSaveHook = options.postSaveHook || function() {};
 
                 // Service object supporting thenable interface with add & save methods
-                var Model       = options.Model;
+                var Model        = options.Model;
 
                 // The local instance of this model 
-                var instance    = options.instance;
+                var instance     = options.instance;
 
                 // The routeParams id
-                var id          = options.id;
+                var id           = options.id;
 
                 // The form to reset to pristine after save
-                var form        = options.form;
+                var form         = options.form;
 
                 // The redirect URL, if doRedirect is set to true
-                var redirectUrl = options.redirectUrl;
+                var redirectUrl  = options.redirectUrl;
 
                 // Whether or not to redirect. If false, the save method will set the form
                 // to pristine and stay on the same page. 
-                var doRedirect  = options.doRedirect || false;
+                var doRedirect   = options.doRedirect || false;
+
+                var formsValid   = true;
 
                 if (!form) return;
 
-                // ********** Private Methods ************ //
-
-                function postSaveLogic(resourceId) {
-                    redirectToNewResource(resourceId);
-                    handleRedirectOverride();
-                    setPristine();
-                }
-
-                function handleRedirectOverride() {
-                    if (doRedirect) { $location.url(redirectUrl); }
-                }
-
-                function redirectToNewResource(resourceId) {
-                    if (resourceId) { $location.url(redirectUrl + '/' + id); }
-                }
-
-                function setPristine(resourceId) {
-                    if (!resourceId) { FormHelper.setPristine(form); }
-                }
-
                 // ******** Execute the Strategy ******** //
 
-                strategy();
+                preSaveHook();
 
                 // ********* Validate the form ********* //
                 // ********* Save the model if ********* //
                 // ********* Valid, or display ********* //
                 // *************** errors ************** //
 
-                if (Object.prototype.toString.call(form) == '[object Array]') {
-                    form.forEach(function(f) { processForm(f); });
-                } else {
-                    processForm(form);
+                if (isArray(form))   { processForms(); }
+                if (isObj(form))     { processForm();  }
+                // ********** Private Methods ************ //
+
+                function isArray(obj) {
+                    return typeOf(obj) == '[object Array]';
                 }
 
-                function processForm(f) {
-                    if (f.$valid) {
-                        successCallback();
-                    } else {
-                        $rootScope.Validator.validateForm(f);
-                    }
+                function isObj(obj) {
+                    return typeOf(obj) == '[object Object]';
                 }
-                
+
+                function typeOf(obj) {
+                    return Object.prototype.toString.call(obj);
+                }
+
+                function processForms() {
+                    form.forEach(function(f) { checkFormValidity(f); });
+                    if (formsValid)          { successCallback(); }
+                    if (!formsValid)         { form.forEach(function(f) { markInvalidFields(f); }); }
+                }
+
+                function checkFormValidity(form) {
+                    if (form.$invalid) { formsValid = false; }
+                }
+
+                function processForm() {
+                    if (form.$valid)   { successCallback(); }
+                    if (form.$invalid) { markInvalidFields(form); }
+                }
+
+                function markInvalidFields(form) {
+                    $rootScope.Validator.validateForm(form);
+                }
+
                 function successCallback() {
                     // ********* Perform Model.add ********* //
                     // ******** if not on show page ******** //
@@ -92,6 +96,25 @@ angular
                         });
 
                     }
+                }
+
+                function postSaveLogic(resourceId) {
+                    redirectToNewResource(resourceId);
+                    handleRedirectOverride();
+                    setPristine();
+                    postSaveHook();
+                }
+
+                function handleRedirectOverride() {
+                    if (doRedirect) { $location.url(redirectUrl); }
+                }
+
+                function redirectToNewResource(resourceId) {
+                    if (resourceId) { $location.url(redirectUrl + '/' + id); }
+                }
+
+                function setPristine(resourceId) {
+                    if (!resourceId) { FormHelper.setPristine(form); }
                 }
                 
             }

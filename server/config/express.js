cfcd2457750ff8f8d8c6e26c00b53cd46304b6a9
Aaron Frey <aaron@facultycreative.com>
2013-10-15 17:51:15 -0400

    Add white bar to the top of the quoter too to accent the logo

--- server/config/express.js
+++ server/config/express.js
@@ -25,8 +25,7 @@ module.exports = function(app, config, passport, standardReponse) {
     var allowCrossDomain = function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Headers', 'Content-Type,X-XSRF-TOKEN');
 
         next();
     };
